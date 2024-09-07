a7bd0858b05464b10688f871e14b93ca7b7c2455
Natalie Gillam <ngillam@hotmail.co.uk>
2024-09-07 15:22:15 +0100

    updated questions and answers in index.js

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const quizData = [
-    {
-        id: 1,
-        question: "Is this sentence an exclamation or a question? 'How disappointing it was that it rained on sports day.'",
-        options: ["Exclamation", "Question"],
-        correct: 0,
-        questionRef: "paper1-2023-2a",
-      },
-      {
-        id: 2,
-        question: "Is this sentence an exclamation or a question? 'How many times have you been to the swimming pool this week'",
-        options: ["Exclamation", "Question"],
-        correct: 1,
-        questionRef: "paper1-2023-2b",
-      },
-]
+  {
+    id: 1,
+    // question: "Is this sentence an exclamation or a question? 'How many times have you been to the swimming pool this week'",
+    options: ["wrighting", "writing"],
+    correct: 1,
+    questionRef: "english",
+  },
+  {
+    id: 2,
+    // question: "Is this sentence an exclamation or a question? 'How many times have you been to the swimming pool this week'",
+    options: ["punctuation", "puctuation"],
+    correct: 0,
+    questionRef: "english",
+  },
+];
 
-const answerElement = document.querySelectorAll('.answer');
-const [questionElement, option_1, option_2, option_3, option_4] = document.querySelectorAll('#question,  .option_1, .option_2, .option_3, .option_4'); 
-const submitBtn = document.querySelector('#submit');
+const answerElement = document.querySelectorAll(".answer");
+const [option_1, option_2] = document.querySelectorAll(
+  ".option_1, .option_2"
+);
+const submitBtn = document.querySelector("#submit");
 
-const currentQuiz = 0; 
-const score = 0; 
+const currentQuiz = 0;
+const score = 0;
 
 const loadQuiz = () => {
-const {question, options } = quizData[currentQuiz];
-console.log(options)
-questionElement.innerText = question;
-options.forEach((curOption, index) => window[`option_${index+1}`].innerText = curOption) 
-}
+  const { question, options } = quizData[currentQuiz];
+  console.log(options);
+  // questionElement.innerText = question;
+  options.forEach(
+    (curOption, index) => (window[`option_${index + 1}`].innerText = curOption)
+  );
+};
 
 loadQuiz();
